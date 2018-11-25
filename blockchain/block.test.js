c058787740a493cc4ee25346fb14fc05d3df38ac
fuki watanabe <fu.watanabe@live.jp>
2018-11-25 15:13:39 +0900

    Add proof of work logic

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -15,4 +15,17 @@ describe('Block', () => {
     it('set the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
+
+    it('generates a hash that match the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+        console.log(block.toString());
+    });
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty-1);
+    });
+
+    it('raises the difficulty for quickly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+1)).toEqual(block.difficulty+1);
+    });
 });
